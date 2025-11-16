abce4cfe7e9d1781e2dcee1be520f427ad33953f
6et3ct1ve <6et3ct1ve@gmail.com>
2025-11-16 13:18:32 +0200

    Correcting model in reviews

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from games.models import Game
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Review(models.Model):
@@ -8,11 +9,11 @@ class Review(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reviews"
     )
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="reviews")
-    gameplay = models.IntegerField()
-    graphics = models.IntegerField()
-    story = models.IntegerField()
-    sound = models.IntegerField()
-    replayability = models.IntegerField()
+    gameplay = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    graphics = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    story = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    sound = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    replayability = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
