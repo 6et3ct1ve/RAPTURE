e95beafc90ce6df833e3f23d004711bbad278808
6et3ct1ve <6et3ct1ve@gmail.com>
2025-11-19 01:44:03 +0200

    Games app

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "role", "unique_id", "created_at"]
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import User
+from .serializers import (
+    UserRegisterSerializer,
+    UserListSerializer,
+    UserUpdateSerializer,
+)
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def get_serializer_class(self):
+        if self.request.method == "PATCH":
+            return UserUpdateSerializer
+        return UserListSerializer
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
+    permission_classes = [AllowAny]
